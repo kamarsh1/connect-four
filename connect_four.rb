12d2821ceaeb7277f154d31fe503cf7eb82f9077
Karen Marsh <kamarsh1@hotmail.com>
2018-02-10 10:04:45 -0500

    Create spec and game.

---
+++ connect_four.rb
@@ -0,0 +1,25 @@
+class Game
+  # attr_accessor :column
+  attr_accessor :game_board, :game_over, :player1, :valid_move
+
+  def initialize
+    @game_board = Array.new(6){Array.new(7, '...')}
+    @game_over = false
+    @player1 = false
+  #   @column = column
+  end
+
+  def play
+    # until @game_over
+    @player1 = !@player1
+    @valid_move = false
+    display_board
+  end
+
+  def display_board
+
+  end
+end
+
+game = Game.new
+game.play
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../connect_four'
+
+describe 'Game' do
+  let(:game) { Game.new }
+
+  describe '#initialize' do
+    it 'creates the game board' do
+      expect(game.game_board).to eq([['...', '...', '...', '...', '...', '...', '...'], ['...', '...', '...', '...', '...', '...', '...'], ['...', '...', '...', '...', '...', '...', '...'], ['...', '...', '...', '...', '...', '...', '...'], ['...', '...', '...', '...', '...', '...', '...'], ['...', '...', '...', '...', '...', '...', '...']])
+    end
+
+    it 'sets game over flag' do
+      expect(game.game_over).to eq(false)
+    end
+
+    it 'sets player1' do
+      expect(game.player1).to eq(false)
+    end
+  end
+
+  describe '#play' do
+    describe 'until the game is over' do
+      before do
+        allow(game).to receive(:display_board)
+      end
+
+      it 'toggles player1' do
+        player1_toggle = !game.player1
+        game.play
+        expect(game.player1).to eq(player1_toggle)
+      end
+
+      it 'sets valid move' do
+        game.play
+        expect(game.valid_move).to eq(false)
+      end
+
+      it 'displays the board' do
+        game.play
+        expect(game).to have_received(:display_board)
+      end
+    end
+  end
+end
