dea838a057f5a786762b5b608a25653e81f70a7f
Karen Marsh <kamarsh1@hotmail.com>
2018-02-11 17:59:11 -0500

    Uncomment necessary tests & delete commented tests that were replaced in the last commit

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -83,184 +83,138 @@ describe 'Game' do
     end
   end
 
+  describe '#display_board' do
+    let(:board) {"\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n\n"}
 
+    it 'prints out the game board' do
+      expect { game.display_board }.to output(board).to_stdout
+    end
+  end
 
-#       describe 'when it IS a valid move' do
-#         before do
-#           allow(game).to receive(:valid_move?).and_return(true)
-#           allow(game).to receive(:place_chip_in_column)
-#           game.play
-#         end
-#         it 'places chip in column' do
-#           expect(game).to have_received(:place_chip_in_column)
-#         end
-#       end
-#
-#       describe 'when it is NOT a valid move' do
-#         before do
-#           allow(game).to receive(:invalid_selection)
-#           allow(game).to receive(:place_chip_in_column)
-#           allow(game).to receive(:valid_move?).and_return(false, true)
-#
-#           game.play
-#         end
-#
-#         it 'loops until it IS a valid move' do
-#           expect(game).to have_received(:valid_move?).twice
-#           expect(game).to have_received(:invalid_selection)
-#           expect(game).to have_received(:pick_a_column).twice
-#           expect(game).to have_received(:place_chip_in_column)
-#
-#         end
-#         # it 'displays invalid selection' do
-#         #   expect(game).to have_received(:invalid_selection)
-#         # end
-#         #
-#         # it 'picks a column again' do
-#         #
-#         #
-#         # end
-#         # it 'places chip in column for valid move' do
-#         #   expect(game).to have_received(:place_chip_in_column)
-#         #
-#         # end
-#       end
-# end
+  describe '#pick_a_column' do
+    context 'when player1' do
+      before do
+        game.player1 = true
+      end
 
+      it 'asks player to pick a column' do
+        expect { game.pick_a_column }.to output("Pick a column (1 through 7)\n").to_stdout
+      end
 
+      it 'gets a number from input' do
+        allow(game).to receive(:gets).and_return('3')
+        expect(game.pick_a_column).to eq(3)
+      end
+    end
 
+    context 'when NOT player1' do
+      before do
+        game.player1 = false
+      end
 
-  # describe '#display_board' do
-  #   let(:board) {"\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n[\"...\", \"...\", \"...\", \"...\", \"...\", \"...\", \"...\"]\n\n"}
-  #
-  #   it 'prints out the game board' do
-  #     expect { game.display_board }.to output(board).to_stdout
-  #   end
-  # end
-  #
-  # describe '#pick_a_column' do
-  #   context 'when player1' do
-  #     before do
-  #       game.player1 = true
-  #     end
-  #
-  #     it 'asks player to pick a column' do
-  #       expect { game.pick_a_column }.to output("Pick a column (1 through 7)\n").to_stdout
-  #     end
-  #
-  #     it 'gets a number from input' do
-  #       allow(game).to receive(:gets).and_return('3')
-  #       expect(game.pick_a_column).to eq(3)
-  #     end
-  #   end
-  #
-  #   context 'when NOT player1' do
-  #     before do
-  #       game.player1 = false
-  #     end
-  #
-  #     it 'picks a random number between 1 and 7' do
-  #       expect(game.pick_a_column).to be_between(1, 7)
-  #     end
-  #
-  #     # it 'prints a message' do       #### figure this out!!!
-  #     #   expect { game.pick_a_column }.to output("Computer picked").to_stdout
-  #     # end
-  #   end
-  # end
-  #
-  # describe '#valid_move?' do
-  #   context 'column chosen is a valid column' do
-  #     let(:column) { 4 }
-  #
-  #     context 'and the column is NOT full' do
-  #       before do
-  #         game.game_board[5][column-1] = 'BLK'
-  #         game.game_board[4][column-1] = 'BLK'
-  #         game.game_board[3][column-1] = 'BLK'
-  #       end
-  #
-  #       it 'move IS valid' do
-  #         expect(game.valid_move?(column)).to eq(true)
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'column chosen IS a valid column' do
-  #     let(:column) { 7 }
-  #
-  #     context 'but the column is full' do
-  #       before do
-  #         game.game_board[5][column-1] = 'BLK'
-  #         game.game_board[4][column-1] = 'BLK'
-  #         game.game_board[3][column-1] = 'BLK'
-  #         game.game_board[2][column-1] = 'RED'
-  #         game.game_board[1][column-1] = 'RED'
-  #         game.game_board[0][column-1] = 'RED'
-  #       end
-  #
-  #       it 'move is NOT valid' do
-  #         expect(game.valid_move?(column)).to eq(false)
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'when column chosen is NOT valid' do
-  #     let(:column) { 12 }
-  #     it 'returns false when NOT a valid move' do
-  #       expect(game.valid_move?(column)).to eq(false)
-  #     end
-  #   end
-  # end
-  #
-  # describe '#place_chip_in_column' do
-  #   let(:column) { 1 }
-  #
-  #   context 'when player1' do
-  #     before do
-  #       game.player1 = true
-  #       game.game_board[5][column-1] = '...'
-  #     end
-  #
-  #     it 'places a RED chip in bottom-most empty row for chosen column' do
-  #       game.place_chip_in_column(column)
-  #       expect(game.game_board[5][column-1]).to eq('RED')
-  #     end
-  #   end
-  #
-  #   context 'when NOT player1' do
-  #     before do
-  #       game.player1 = false
-  #       game.game_board[5][column-1] = '...'
-  #     end
-  #
-  #     it 'places a BLK chip in bottom-most empty row for chosen column' do
-  #       game.place_chip_in_column(column)
-  #       expect(game.game_board[5][column-1]).to eq('BLK')
-  #     end
-  #   end
-  #
-  #   context 'when lower rows for the chosen column are full' do
-  #     before do
-  #       game.player1 = true
-  #       game.game_board[5][column-1] = 'BLK'
-  #       game.game_board[4][column-1] = 'BLK'
-  #       game.game_board[3][column-1] = 'BLK'
-  #     end
-  #
-  #     it 'puts chip in next available row for the chosen column' do
-  #       game.place_chip_in_column(column)
-  #       expect(game.game_board[5][column-1]).not_to eq('RED')
-  #       expect(game.game_board[4][column-1]).not_to eq('RED')
-  #       expect(game.game_board[3][column-1]).not_to eq('RED')
-  #       expect(game.game_board[2][column-1]).to eq('RED')
-  #     end
-  #   end
-  # end
-  #
-  # describe '#invalid_selection' do
-  #   it 'prints a message' do
-  #     expect { game.invalid_selection }.to output("Invalid Selection\n").to_stdout
-  #   end
-  # end
+      it 'picks a random number between 1 and 7' do
+        expect(game.pick_a_column).to be_between(1, 7)
+      end
+
+      # it 'prints a message' do       #### figure this out!!!
+      #   expect { game.pick_a_column }.to output("Computer picked").to_stdout
+      # end
+    end
+  end
+
+  describe '#valid_move?' do
+    context 'when column chosen is a valid column' do
+      let(:column) { 4 }
+
+      context 'and the column is NOT full' do
+        before do
+          game.game_board[5][column-1] = 'BLK'
+          game.game_board[4][column-1] = 'BLK'
+          game.game_board[3][column-1] = 'BLK'
+        end
+
+        it 'the move IS valid' do
+          expect(game.valid_move?(column)).to eq(true)
+        end
+      end
+    end
+
+    context 'when column chosen IS a valid column' do
+      let(:column) { 7 }
+
+      context 'but the column is full' do
+        before do
+          game.game_board[5][column-1] = 'BLK'
+          game.game_board[4][column-1] = 'BLK'
+          game.game_board[3][column-1] = 'BLK'
+          game.game_board[2][column-1] = 'RED'
+          game.game_board[1][column-1] = 'RED'
+          game.game_board[0][column-1] = 'RED'
+        end
+
+        it 'the move is NOT valid' do
+          expect(game.valid_move?(column)).to eq(false)
+        end
+      end
+    end
+
+    context 'when column chosen is NOT valid' do
+      let(:column) { 12 }
+      
+      it 'the move is NOT valid' do
+        expect(game.valid_move?(column)).to eq(false)
+      end
+    end
+  end
+
+  describe '#place_chip_in_column' do
+    let(:column) { 1 }
+
+    context 'when player1' do
+      before do
+        game.player1 = true
+        game.game_board[5][column-1] = '...'
+      end
+
+      it 'places a RED chip in bottom-most empty row for chosen column' do
+        game.place_chip_in_column(column)
+        expect(game.game_board[5][column-1]).to eq('RED')
+      end
+    end
+
+    context 'when NOT player1' do
+      before do
+        game.player1 = false
+        game.game_board[5][column-1] = '...'
+      end
+
+      it 'places a BLK chip in bottom-most empty row for chosen column' do
+        game.place_chip_in_column(column)
+        expect(game.game_board[5][column-1]).to eq('BLK')
+      end
+    end
+
+    context 'when lower rows for the chosen column are full' do
+      before do
+        game.player1 = true
+        game.game_board[5][column-1] = 'BLK'
+        game.game_board[4][column-1] = 'BLK'
+        game.game_board[3][column-1] = 'BLK'
+      end
+
+      it 'puts chip in next available row for the chosen column' do
+        game.place_chip_in_column(column)
+        expect(game.game_board[5][column-1]).not_to eq('RED')
+        expect(game.game_board[4][column-1]).not_to eq('RED')
+        expect(game.game_board[3][column-1]).not_to eq('RED')
+        expect(game.game_board[2][column-1]).to eq('RED')
+      end
+    end
+  end
+
+  describe '#invalid_selection' do
+    it 'prints a message' do
+      expect { game.invalid_selection }.to output("Invalid Selection\n").to_stdout
+    end
+  end
 end
